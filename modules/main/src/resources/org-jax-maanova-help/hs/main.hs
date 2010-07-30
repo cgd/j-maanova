823c05931db15e7c8878d4dddb938fe00f894a6a
Keith Sheppard <keithshep@gmail.com>
2010-07-30 11:53:25 -0400

    adding javahelp files to the repo. i didnt want to do this since these are machine generated but i am having some trouble getting helpsetmaker to build the artifacts for some reason. also the helpsetmaker isnt integrated with ant

---
+++ modules/main/src/resources/org-jax-maanova-help/hs/main.hs
@@ -0,0 +1,23 @@
+<?xml version='1.0' encoding='UTF-8' ?>
+<!DOCTYPE helpset PUBLIC "-//Sun Microsystems Inc.//DTD JavaHelp HelpSet Version 2.0//EN" "http://java.sun.com/products/javahelp/helpset_2_0.dtd">
+<helpset version="2.0">
+<title>J/maanova 1.0.0</title>
+<maps>
+<homeID>first-topic</homeID>
+<mapref location="map.jhm" />
+</maps>
+<view xml:lang="ar" mergetype="javax.help.UniteAppendMerge">
+  <name>TOC</name>
+  <label>helpset.toc.title</label>
+  <type>javax.help.TOCView</type>
+  <data>toc.xml</data>
+</view>
+<view xml:lang="ar">
+<name>Search</name>
+<label>helpset.search.title</label>
+<type>javax.help.SearchView</type>
+<data engine="com.sun.java.help.search.DefaultSearchEngine">
+JavaHelpSearch
+</data>
+</view>
+</helpset>
